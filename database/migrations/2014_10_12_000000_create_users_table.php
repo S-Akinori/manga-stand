d4e6a4dbbc8fd550343b6deda02d39cbfe4528eb
Akinori <aki10.working@gmail.com>
2021-05-27 10:36:05 +0900

    first commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->timestamp('email_verify_token')->nullable();
+            $table->string('email_verify_token')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->string('description');
